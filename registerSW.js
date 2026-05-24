22c67876f7494dcf46e4d590fffcd0088b969e27
alfiankan <alfiankan@users.noreply.github.com>
2026-05-24 12:09:59 +0000

    deploy: f0ed9a2a6a053d902218a23f9e97ccde4861a001

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gimanasih/sw.js', { scope: '/gimanasih/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
